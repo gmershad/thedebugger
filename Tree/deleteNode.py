f9984102edbf33d098f805910138ad13915c0d50
Gul Ershad <mailtogulershad@gmail.com>
2021-12-27 13:32:31 +0530

    Create deleteNode.py

---
+++ Tree/deleteNode.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return root
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+        
+            temp = self.minValueNode(root.right)
+            root.val = temp.val
+            root.right = self.deleteNode(root.right, temp.val)
+        
+        return root
+
+    
+    def minValueNode(self, node):
+        current = node
+        
+        while current.left is not None:
+            current = current.left
+        
+        return current
+    
