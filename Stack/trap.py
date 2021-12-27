1fad2af98372f399390322c2fe7b849f12690aab
Gul Ershad <mailtogulershad@gmail.com>
2021-12-27 11:00:58 +0530

    Create trap.py

---
+++ Stack/trap.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        n = len(height)
+        if n < 3:
+            return 0
+        
+        result, left, right = 0, 1, n-2
+        left_max = height[0]
+        right_max = height[n-1]
+        
+        
+        while left <= right:
+            if left_max <= right_max:
+                left_max = max(left_max, height[left])
+                result += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                result += right_max - height[right]
+                right -= 1
+
+        return result
+        
