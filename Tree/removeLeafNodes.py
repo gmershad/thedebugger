af7bb6966405a5c3080ff60a7f94a93d96ec3f85
Gul Ershad <mailtogulershad@gmail.com>
2021-12-27 17:04:34 +0530

    Create removeLeafNodes.py

---
+++ Tree/removeLeafNodes.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def removeLeafNodes(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode
+        """
+        if root:
+            root.left = self.removeLeafNodes(root.left, target)
+            root.right = self.removeLeafNodes(root.right, target)
+            
+            if root.val == target and root.left == None and root.right == None:
+                root = None
+                
+        return root
