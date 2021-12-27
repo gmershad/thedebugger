1e7f65b3173ab1e554310d1688196969d6215935
Gul Ershad <mailtogulershad@gmail.com>
2021-12-27 13:39:24 +0530

    Create findTilt.py

---
+++ Tree/findTilt.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findTilt(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def helper(root):
+            if not root: return 0
+            lv, rv = helper(root.left), helper(root.right)
+            self.ans += abs(lv - rv)
+            return root.val + lv + rv
+        
+        self.ans = 0
+        helper(root)
+        return self.ans
