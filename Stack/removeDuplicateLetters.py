35ba673d0e659140983df209e0e9a833a4d3d6a6
Gul Ershad <mailtogulershad@gmail.com>
2021-12-27 11:15:21 +0530

    Create removeDuplicateLetters.py

---
+++ Stack/removeDuplicateLetters.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        d = {char: indx for indx, char in enumerate(s)}
+        
+        res = []
+        
+        for indx, char in enumerate(s):
+            if char not in res:
+                
+                while res and indx < d[res[-1]] and char < res[-1]:
+                    res.pop()
+                    
+                res.append(char)
+        
+        return "".join(res)
+        
