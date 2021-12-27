626bec5f2e48014443d2afed79672112864e914b
Gul Ershad <mailtogulershad@gmail.com>
2021-12-27 13:56:23 +0530

    Create KthLargest.py

---
+++ Tree/KthLargest.py
@@ -0,0 +1,33 @@
+import heapq
+
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.heap = []
+        self.k = k
+        
+        for num in nums:
+            self.add(num)
+        
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        heapq.heappush(self.heap, val)
+        
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        
+        return self.heap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
