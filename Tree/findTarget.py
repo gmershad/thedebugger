2af0a89122583a72ac9acdc3b1fbf52b38c1e850
Gul Ershad <mailtogulershad@gmail.com>
2021-12-27 13:47:55 +0530

    Create findTarget.py

---
+++ Tree/findTarget.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        # Store k - vals we come across in our vals dict (O(1) lookup).
+        vals = {}
+        
+        def helper(node):
+		    # If we run out of nodes on this path we know there was no val, False.
+            if not node:
+                return False
+			# If we come across a val needed to make the sum, return True.
+            if node.val in vals:
+                return True
+			# If none of the above, add k - node.val to vals.
+            vals[k - node.val] = True
+			# Return if we were able to find a path in either subtree.
+            return helper(node.left) or helper(node.right)
+        
+        return helper(root)
